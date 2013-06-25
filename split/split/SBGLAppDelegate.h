dcf69788c136982a2b3c29e109253388f7be16b4
AdamGluck <adamt.gluck@gmail.com>
2013-06-25 16:45:22 -0500

    initial commit

---
+++ split/split/SBGLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SBGLAppDelegate.h
+//  split
+//
+//  Created by Adam Gluck on 6/25/13.
+//  Copyright (c) 2013 BGL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBGLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ split/split/SBGLViewController.h
@@ -0,0 +1,13 @@
+//
+//  SBGLViewController.h
+//  split
+//
+//  Created by Adam Gluck on 6/25/13.
+//  Copyright (c) 2013 BGL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBGLViewController : UIViewController
+
+@end
