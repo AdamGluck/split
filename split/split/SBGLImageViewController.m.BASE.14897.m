1ae86c598cad11827ab0569ae5997a4294e8aa5d
AdamGluck <adamt.gluck@gmail.com>
2013-06-25 23:58:17 -0500

    update

---
+++ split/split/SBGLImageViewController.m.BASE.14897.m
@@ -0,0 +1,29 @@
+//
+//  SBGLViewController.m
+//  split
+//
+//  Created by Adam Gluck on 6/25/13.
+//  Copyright (c) 2013 BGL. All rights reserved.
+//
+
+#import "SBGLViewController.h"
+
+@interface SBGLImageViewController ()
+
+@end
+
+@implementation SBGLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
